e77344c83c3d5d7a0d7c8a6b889e23cfe5054399
Nathan Sanchez <nathanryan.sanchez@calbaptist.edu>
2022-11-08 18:15:00 -0800

    finished basic frontend, now time for js

---
+++ script.js
@@ -0,0 +1,4 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c7efc17d983cc88248a2487b6be53283&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=c7efc17d983cc88248a2487b6be53283&query='
+
